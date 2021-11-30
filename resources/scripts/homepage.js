ccbe4c8cbe2c53197e1edc80ab1a592f7f0ed96e
Hanna Rosenmertz <hannarosenmertz@Hannas-MacBook-Pro-2.local>
2021-11-29 18:00:32 -0600

    home

--- resources/scripts/homepage.js
+++ resources/scripts/homepage.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://cts-client.herokuapp.com/";
+const baseUrl = "file:///Users/hannarosenmertz/Desktop/Source/Repos/cts/client/homepage.html";
 
 function loadHomePage(){
     const peopleUrl = baseUrl;
