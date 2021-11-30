7f1d711cd3c8bb32a0c6e4a7c2d5e7785b35b9ee
Hanna Rosenmertz <hannarosenmertz@Hannas-MacBook-Pro-2.local>
2021-11-30 12:45:34 -0600

    login2

--- resources/scripts/login.js
+++ resources/scripts/login.js
@@ -6,8 +6,6 @@ function handleLogin(){
 function PostRequest(){
     const userApiUrl = "https://cts-api-321.herokuapp.com/api/Client/";
     let value = document.getElementById("value").value; //gets what user inputted 
-    const username;
-    const password;
     fetch(userApiUrl, {
         method: "POST",
         headers: {
