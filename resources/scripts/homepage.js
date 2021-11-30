60a6c33bb508579615bb2e0950559fff514e548c
Hanna Rosenmertz <hannarosenmertz@Hannas-MacBook-Pro-2.local>
2021-11-29 18:15:28 -0600

    home2

--- resources/scripts/homepage.js
+++ resources/scripts/homepage.js
@@ -3,11 +3,14 @@ const baseUrl = "https://cts-client.herokuapp.com/";
 function loadHomePage(){
     const peopleUrl = baseUrl;
 
-    fetch(peopleUrl).then(function(response){
-        return response.json();
-    }).then(function(json){
-        console.log(json);
-    }).catch(function(error){
-        console.log(error);
-    });
+    fetch(peopleUrl)
+    
+    
+    // .then(function(response){
+    //     return response.json();
+    // }).then(function(json){
+    //     console.log(json);
+    // }).catch(function(error){
+    //     console.log(error);
+    // });
 };
