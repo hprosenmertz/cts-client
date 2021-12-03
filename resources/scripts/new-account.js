8a26ba1e9ce75c6d112b6630e25743781adeb0b0
Hanna Rosenmertz <hannarosenmertz@Hannas-MacBook-Pro-2.local>
2021-12-03 12:51:45 -0600

    new

--- resources/scripts/new-account.js
+++ resources/scripts/new-account.js
@@ -10,20 +10,75 @@ function handleOnLoad(){
 }
 
 function handleOnSubmit(){
-    AddAccount();
+    AddClient();
 }
 
 function AddAccount(){
+    // console.log("dfghjkmnbvcdfghjkhgfvb vghju")
 
-    const apiUrl = "https://cts-api-321.herokuapp.com/api/Client/"; 
-    let username = document.getElementById("username").value;
+    // const apiUrl = "https://cts-api-321.herokuapp.com/api/Client"; 
+    // let username = document.getElementById("email").value;
+    // let password = document.getElementById("password").value; //gets what user inputted 
+    // let firstName = document.getElementById("fName").value;
+    // let lastName = document.getElementById("lName").value;
+    // let phone = document.getElementById("phone").value;
+
+    // console.log(username )
+    // console.log( password )//gets what user inputted 
+    // console.log( firstName, lastName, phone )
+
+    // {
+    //     clientFirstName: firstName,
+    //     clientLastName: lastName,
+    //     clientEmail: username,
+    //     clientPass: password,
+    //     phone: phone
+    // }
+
+    // fetch(apiUrl, {
+    //     method: "POST",
+    //     headers: {
+    //         "Accept": 'application/json',
+    //         "Content-Type": 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //             clientFirstName: firstName,
+    //             clientLastName: lastName,
+    //             clientEmail: username,
+    //             clientPass: password,
+    //             phone: phone
+    //         })
+    // })
+    // .then((response)=> response.json()).then(num =>{
+    //     if (num === 0){
+    //         alert("Invalid Account Creation, please try again");
+    //         localStorage.setItem("client",0);
+    //     } else {
+    //         alert("Success, account created!");
+    //         localStorage.setItem("client", num);
+    //         document.getElementById("email").value = "";
+    //         document.getElementById("password").value = "";
+    //         document.getElementById("fName").value = "";
+    //         document.getElementById("lName").value = "";
+    //         document.getElementById("phone").value = "";
+    //         //handleOnLoad();
+    //     }
+        
+    // })
+    // .catch(error => console.log(error));
+
+}
+
+function AddClient(){
+    const apiUrl = "https://cts-api-321.herokuapp.com/api/Client"; 
+
+    let username = document.getElementById("email").value;
     let password = document.getElementById("password").value; //gets what user inputted 
-    let firstName = document.getElementById("firstName").value;
-    let lastName = document.getElementById("lastName").value;
+    let firstName = document.getElementById("fName").value;
+    let lastName = document.getElementById("lName").value;
     let phone = document.getElementById("phone").value;
 
-
-    fetch(postApiUrl, {
+    fetch(apiUrl, {
         method: "POST",
         headers: {
             "Accept": 'application/json',
@@ -37,22 +92,18 @@ function AddAccount(){
             phone: phone
         })
     })
-    .then((response)=> response.json()).then(num =>{
-        if (num === 0){
-            alert("Invalid Account Creation, please try again");
-            localStorage.setItem("client",0);
-        } else {
-            alert("Success, account created!");
-            localStorage.setItem("client", num);
-            document.getElementById("username").value = "";
-            document.getElementById("password").value = "";
-            document.getElementById("firstName").value = "";
-            document.getElementById("lastName").value = "";
-            document.getElementById("phone").value = "";
-            handleOnLoad();
-        }
-        
+    .then((response)=> {
+        console.log(response);
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+        document.getElementById("fName").value = "";
+        document.getElementById("lName").value = "";
+        document.getElementById("phone").value = "";
+        // handleOnLoad();
     })
-    .catch(error => console.log(error));
+
+
+
+
 
 }
