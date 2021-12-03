46120014c6bb90597524697880d0b78496245a23
Hanna Rosenmertz <hannarosenmertz@Hannas-MacBook-Pro-2.local>
2021-12-03 17:38:08 -0600

    hp

--- resources/scripts/employee-home.js
+++ resources/scripts/employee-home.js
@@ -1,10 +1,120 @@
-
+const postApiUrl = "https://cts-api-321.herokuapp.com/api/Event/";
 // Add a "checked" symbol when clicking on a list item
-function CheckList(){
+function CheckList(events){
+
     var list = document.querySelector('ul');
+
+    var list = document.getElementById("myUL");
+
+    var assigned;
+    var confirmed;
+    var dos;
+    var setup;
+    var eip;
+    var tearDown;
+    var complete;
+
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
+    console.log(ev.target.id);
+   
+    var clicked = ev.target.id;
+    var tabId = clicked.split("-").pop();
+    console.log(tabId);
+
     ev.target.classList.toggle('checked');
+
+    if(clicked.includes("confirmed")){
+      if(ev.target.className === "checked"){
+          confirmed = true;
+      }
+      else{
+          confirmed = false;
+      }
+    }
+    if(clicked.includes("assigned")){
+      if(ev.target.className === "checked"){
+          assigned = true;
+      }
+      else{
+          assigned = false;
+      }
+    }
+    if(clicked.includes("dayOfStatus")){
+      if(ev.target.className === "checked"){
+          dos = true;
+      }
+      else{
+          dos = false;
+      }
+    }
+    if(clicked.includes("setupCompleted")){
+      if(ev.target.className === "checked"){
+          setup = true;
+      }
+      else{
+          setup = false;
+      }
+    }
+    if(clicked.includes("inProgress")){
+      if(ev.target.className === "checked"){
+          eip = true;
+      }
+      else{
+          eip = false;
+      }
+    }
+    if(clicked.includes("tearDown")){
+      if(ev.target.className === "checked"){
+          tearDown = true;
+      }
+      else{
+          tearDown = false;
+      }
+    }
+    if(clicked.includes("complete")){
+      if(ev.target.className === "checked"){
+          complete = true;
+      }
+      else{
+          complete = false;
+      }
+    }
+    
+  //  const thisEvent = fetch(`${postApiUrl}/${tabId}`).then((res) => res.json());
+  //   thisEvent.then((event => {
+  //     event.confirmed = confirmed;
+  //     event.assigned = assigned;
+  //     event.dayOfStatus = dos;
+  //     event.setupCompleted = setup;
+  //     event.inProgress = eip;
+  //     event.tearDown = tearDown;
+  //     event.complete = complete;
+  //   }))
+
+    fetch(postApiUrl,{
+      method: "PUT",
+      headers: {
+          "Accept": 'application/json',
+          "Content-Type": 'application/json'
+      },
+      body: JSON.stringify({
+      
+        // thisEvent
+          eventId: tabId,
+          confirmed: confirmed,
+          assigned: assigned,
+          dayOfStatus: dos,
+          setupCompleted: setup,
+          inProgress: eip,
+          tearDown: tearDown,
+          complete: complete
+    })
+      
+    })
+      .then(function(response){
+        return response.json();
+    })
   }
 }, false);
 
@@ -12,36 +122,79 @@ list.addEventListener('click', function(ev) {
 
 
 function handleOnLoad(){
-  DisplayEvents();
-  const postApiUrl = "https://cts-api-321.herokuapp.com/api/Event/";
 
   fetch(postApiUrl).then(function(response){   
-      return response.json();
+    return response.json();
   }).then(function(json){
-      console.log(json);
+    console.log(json);
+    DisplayEvents(json);
+    CheckList(json);
   })
-    CheckList();
   
 }
 
-//need method to update back end
-
 //method to show all events for employee
-function DisplayEvents(){
+function DisplayEvents(json){
 
   let html = "<body>";
+  var list = document.getElementById("myUL")
 
-  json.forEach(text => {
+  json.forEach(event => {
       html = "<div class=\"box\">";
       html += "<div id=\"myDIV\" class=\"header\">";
-      html += "<h3 style=\"margin:5px\">Event To Do List</h3>"
-      html += "</div>"
-      html += "</div>"
+      html += "<h3 style=\"margin:5px\">" + event.eventId + "</h3>";
+      html += "</div>";
+      html += "<ul id=\"myUL\">";
+      if(event.confirmed){
+        html += "<li id=\"confirmed-" + event.eventId + "\" class=\"checked\">Confirmed</li>";
+      }
+      else{
+        html += "<li id=\"confirmed-" + event.eventId + "\">Confirmed</li>";
+      }
+      if(event.assigned){
+        html += "<li id=\"assigned-" + event.eventId + "\" class=\"checked\">Assigned</li>";
+      }
+      else{
+        html += "<li id=\"assigned-" + event.eventId + "\">Assigned</li>";
+      }
+      if(event.dayOfStatus){
+        html += "<li id=\"dayOfStatus-" + event.eventId + "\" class=\"checked\">\"Day of\" Status</li>";
+      }
+      else{
+        html += "<li id=\"dayOfStatus-" + event.eventId + "\">\"Day of\" Status</li>";
+      }
+      if(event.setupCompleted){
+        html += "<li id=\"setupCompleted-" + event.eventId + "\" class=\"checked\">Setup Completed</li>";
+      }
+      else{
+        html += "<li id=\"setupCompleted-" + event.eventId + "\">Setup Completed</li>";
+      }
+      if(event.inProgress){
+        html += "<li id=\"inProgress-" + event.eventId + "\" class=\"checked\">Event In Progress</li>";
+      }
+      else{
+        html += "<li id=\"inProgress-" + event.eventId + "\">Event In Progress</li>";
+      }
+      if(event.tearDown){
+        html += "<li id=\"tearDown-" + event.eventId + "\" class=\"checked\">Tear Down</li>";
+      }
+      else{
+        html += "<li id=\"tearDown-" + event.eventId + "\">Tear Down</li>";
+      }
+      if(event.complete){
+        html += "<li id=\"complete-" + event.eventId + "\" class=\"checked\">Complete</li>";
+      }
+      else{
+        html += "<li id=\"complete-" + event.eventId + "\">Complete</li>";
+      }
+
+      html += "</ul>";
+      html += "</div>";
  
-      
   });
   
-  html += "</div>";
+  html += "</body>";
+  document.getElementById("myUL").innerHTML = html;
 
-  document.getElementById("prevposts").innerHTML = html;
+  // document.getElementById("prevposts").innerHTML = html;
 }
--- resources/scripts/new-account.js
+++ resources/scripts/new-account.js
@@ -13,61 +13,6 @@ function handleOnSubmit(){
     AddClient();
 }
 
-function AddAccount(){
-    // console.log("dfghjkmnbvcdfghjkhgfvb vghju")
-
-    // const apiUrl = "https://cts-api-321.herokuapp.com/api/Client"; 
-    // let username = document.getElementById("email").value;
-    // let password = document.getElementById("password").value; //gets what user inputted 
-    // let firstName = document.getElementById("fName").value;
-    // let lastName = document.getElementById("lName").value;
-    // let phone = document.getElementById("phone").value;
-
-    // console.log(username )
-    // console.log( password )//gets what user inputted 
-    // console.log( firstName, lastName, phone )
-
-    // {
-    //     clientFirstName: firstName,
-    //     clientLastName: lastName,
-    //     clientEmail: username,
-    //     clientPass: password,
-    //     phone: phone
-    // }
-
-    // fetch(apiUrl, {
-    //     method: "POST",
-    //     headers: {
-    //         "Accept": 'application/json',
-    //         "Content-Type": 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //             clientFirstName: firstName,
-    //             clientLastName: lastName,
-    //             clientEmail: username,
-    //             clientPass: password,
-    //             phone: phone
-    //         })
-    // })
-    // .then((response)=> response.json()).then(num =>{
-    //     if (num === 0){
-    //         alert("Invalid Account Creation, please try again");
-    //         localStorage.setItem("client",0);
-    //     } else {
-    //         alert("Success, account created!");
-    //         localStorage.setItem("client", num);
-    //         document.getElementById("email").value = "";
-    //         document.getElementById("password").value = "";
-    //         document.getElementById("fName").value = "";
-    //         document.getElementById("lName").value = "";
-    //         document.getElementById("phone").value = "";
-    //         //handleOnLoad();
-    //     }
-        
-    // })
-    // .catch(error => console.log(error));
-
-}
 
 function AddClient(){
     const apiUrl = "https://cts-api-321.herokuapp.com/api/Client"; 
@@ -99,9 +44,12 @@ function AddClient(){
         document.getElementById("fName").value = "";
         document.getElementById("lName").value = "";
         document.getElementById("phone").value = "";
-        // handleOnLoad();
+        GoLogin();
     })
 
+    function GoLogin(){
+        window.location.href = "https://cts-client.herokuapp.com/login.html";
+    }
 
 
 
