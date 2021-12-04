48787f613d9f0852b137be00b760217e1533f3f1
Hanna Rosenmertz <hannarosenmertz@Hannas-MacBook-Pro-2.local>
2021-12-04 10:00:13 -0600

    hp

--- resources/scripts/display-cust-reviews.js
+++ resources/scripts/display-cust-reviews.js
@@ -3,30 +3,30 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
-window.addEventListener('DOMContentLoaded', () => {
-    let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
-    const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
-        const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
-            // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
-            } else {
-                console.log(123);
-                mainNav.classList.remove('is-visible', 'is-fixed');
-            }
-        } else {
-            // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
-            }
-        }
-        scrollPos = currentTop;
-    });
-})
+// window.addEventListener('DOMContentLoaded', () => {
+//     let scrollPos = 0;
+//     const mainNav = document.getElementById('mainNav');
+//     const headerHeight = mainNav.clientHeight;
+//     window.addEventListener('scroll', function() {
+//         const currentTop = document.body.getBoundingClientRect().top * -1;
+//         if ( currentTop < scrollPos) {
+//             // Scrolling Up
+//             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+//                 mainNav.classList.add('is-visible');
+//             } else {
+//                 console.log(123);
+//                 mainNav.classList.remove('is-visible', 'is-fixed');
+//             }
+//         } else {
+//             // Scrolling Down
+//             mainNav.classList.remove(['is-visible']);
+//             if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+//                 mainNav.classList.add('is-fixed');
+//             }
+//         }
+//         scrollPos = currentTop;
+//     });
+// })
 
 function handleOnLoad(){
     const ApiUrl = "http://cts-api-321.herokuapp.com/api/Review";
@@ -45,11 +45,12 @@ function displayPosts(json){
 
     json.forEach(review => {
         html += "<p><b>Review: </b>" + review.text + "</p>";
+        html += "<p>Testing</p>";
     });
     
     html += "</div>";
 
-    document.getElementById("prevposts").innerHTML = html;
+   // document.getElementById("prevposts").innerHTML = html;
 }
 
 // use for new review submission
